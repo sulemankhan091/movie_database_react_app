369dcb2997e8d7f6ee0145f25578ae736cf70be7
Muhammad Suleman Khan <sulemankhan093@gmail.com>
2022-03-01 11:42:26 +0500

    created Globalstyles for the application and completed the header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+// Imported GlobalStyles here
+import { GlobalStyles } from './GlobalStyles';
+
 import React from 'react';
+
+// components we created
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       Start here.
+      <GlobalStyles/>
     </div>
   );
 }
