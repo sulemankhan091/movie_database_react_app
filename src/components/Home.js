10437fabd64198efbb9f63535fbf385c3d0d969d
Muhammad Suleman Khan <sulemankhan093@gmail.com>
2022-03-06 11:10:12 +0500

    created searchBar component and its logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,8 @@ import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
+import SearchBar from "./SearchBar";
+
 // Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
 
@@ -16,20 +18,24 @@ import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
 	// desturcturing props
-	const { state, loading, error } = useHomeFetch();
+	const { state, loading, error, searchTerm, setSearchTerm } =
+		useHomeFetch();
 
 	console.log(state);
 
 	return (
 		<>
-			{state.results[0] ? (
+			{!searchTerm && state.results[0] ? (
 				<HeroImage
 					image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
 					title={state.results[0].original_title}
 					text={state.results[0].overview}
 				/>
 			) : null}
-			<Grid header="Popular Movies">
+			<SearchBar setSearchTerm={setSearchTerm} />
+			<Grid
+				header={searchTerm ? "Search Result" : "Popular Movies"}
+			>
 				{state.results.map((movie) => (
 					<Thumb
 						key={movie.id}
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -1,45 +1,48 @@
-import {useState,useEffect,useRef} from 'react'
+import { useState, useEffect, useRef } from "react";
 
 // API
-import API from '../API';
+import API from "../API";
 
 // intial state is always a good idea to have
 const intialState = {
-    page: 0,
-    results: [],
-    total_pages: 0,
-    total_results: 0
-}
-
+	page: 0,
+	results: [],
+	total_pages: 0,
+	total_results: 0,
+};
 
 export const useHomeFetch = () => {
-    const [state,setState] = useState(intialState);
-    const [loading,setLoading] = useState(false);
-    const [error,setError] = useState(false);
+	const [searchTerm, setSearchTerm] = useState("");
+	const [state, setState] = useState(intialState);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(false);
 
-    const fetchMovies = async (page,searchTerm="") => {
-        try {
-            setError(false)
-            setLoading(true)
-            
-            const movies = await API.fetchMovies(searchTerm,page);
-            setState(prev => ({
-                ...movies,
-                results:
-                page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-            }))
-        } 
-        
-        catch (error) {
-            setError(true);
-        }
-        setLoading(false);
-    }
+	console.log(searchTerm);
 
-    useEffect(()=>{
-        fetchMovies(1);
-    },[])
+	const fetchMovies = async (page, searchTerm = "") => {
+		try {
+			setError(false);
+			setLoading(true);
 
-    return {state,loading,error}
+			const movies = await API.fetchMovies(searchTerm, page);
+			setState((prev) => ({
+				...movies,
+				results:
+					page > 1
+						? [...prev.results, ...movies.results]
+						: [...movies.results],
+			}));
+		} catch (error) {
+			setError(true);
+		}
+		setLoading(false);
+	};
 
-}
+	// initial render and search
+	useEffect(() => {
+		setState(intialState);
+		fetchMovies(1, searchTerm);
+	}, [searchTerm]);
+
+	return { state, loading, error, searchTerm, setSearchTerm };
+};
